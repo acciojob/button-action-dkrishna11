329b894fdb00e62616e6f721f573f55d18cced2a
dkrishna11 <dkrishna051997@gmail.com>
2023-08-04 17:16:24 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  let [show, setShow]=useState(false);
+
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <button onClick={()=>setShow(!show)}>Show</button>
+      {
+        show && <p className="hide show">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      }
     </div>
   );
 }
